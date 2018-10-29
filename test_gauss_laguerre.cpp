5cf1e2571d06ea3875b1725dd81fa11fb88ea363
Ed Smith-Rowland <3dw4rd@verizon.net>
2018-10-29 15:56:34 -0400

    Make hermite, laguerre integral test output nicer.

--- test_gauss_laguerre.cpp
+++ test_gauss_laguerre.cpp
@@ -18,8 +18,12 @@ template<typename _Tp>
     _Tp e;
 
     _Tp
-    operator()(_Tp x)
-    { return (((a * x + b) * x + c ) * x + d ) * x + e; }
+    operator()(_Tp x) const
+    { return (((a * x + b) * x + c) * x + d) * x + e; }
+
+    _Tp
+    integ(_Tp x) const
+    { return x * ((((a / 5 * x + b / 4) * x + c / 3) * x + d / 2) * x + e / 1); }
   };
 
 template<typename _Tp>
@@ -30,14 +34,24 @@ template<typename _Tp>
     _Tp c;
 
     _Tp
-    operator()(_Tp x)
+    operator()(_Tp x) const
     { return a * std::cos(b * x + c); }
+
+    _Tp
+    integ(_Tp x) const
+    { return a * std::sin(b * x + c) / b; }
   };
 
 template<typename _Tp>
   void
-  test_laguerre_quad()
+  test_laguerre_quad(_Tp proto = _Tp{})
   {
+    std::cout.precision(__gnu_cxx::__digits10(proto));
+    std::cout.flags(std::ios::showpoint);
+    auto width = 8 + std::cout.precision();
+
+    std::cout << '\n';
+
     my_f<_Tp> params{ 0.5, 0.0, 0.0, -1.0, 1.3 };
     my_fc<_Tp> cparams{ 1.0, 1.0, 0.0 };
 
@@ -45,11 +59,11 @@ template<typename _Tp>
     //pfooc = __gnu_cxx::gauss_laguerre_prob_integrate<_Tp>(cparams, 29);
 
     auto xfoo2 = __gnu_cxx::gauss_laguerre_integrate<_Tp>(params, 3, 0.0);
-    std::cout << "xfoo2 = " << xfoo2 << '\n';
-    std::cout << "delta = " << xfoo2 - 0.0L << '\n';
+    std::cout << "integral = " << std::setw(width) << xfoo2 << '\n';
+    std::cout << "delta    = " << std::setw(width) << xfoo2 - 0.0L << '\n';
     auto xfooc = __gnu_cxx::gauss_laguerre_integrate<_Tp>(cparams, 29, 0.0);
-    std::cout << "xfooc = " << xfooc << '\n';
-    std::cout << "delta = " << xfooc - 0.0L << '\n';
+    std::cout << "integral = " << std::setw(width) << xfooc << '\n';
+    std::cout << "delta    = " << std::setw(width) << xfooc - 0.0L << '\n';
   }
 
 int
