e229219956820f99b1ca0e552ff603b19026c865
emsr <3dw4rd@verizon.net>
2019-02-12 23:07:12 -0500

    headers for test functions.

--- libstdc++_support/testsuite/ext/special_functions/radpoly/orthonorm.cc
+++ libstdc++_support/testsuite/ext/special_functions/radpoly/orthonorm.cc
@@ -1,3 +1,22 @@
+// -*- C++ -*-
+// Integration utilities for the C++ library testsuite.
+//
+// Copyright (C) 2016-2019 Free Software Foundation, Inc.
+//
+// This file is part of the GNU ISO C++ Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING3.  If not see
+// <http://www.gnu.org/licenses/>.
 
 #include <limits>
 #include <cassert>
@@ -22,11 +41,6 @@ template<typename _Tp>
 	 * __gnu_cxx::radpoly(n1, m1, rho)
 	 * __gnu_cxx::radpoly(n2, m2, rho) / norm;
   }
-
-template<typename _Tp>
-  _Tp
-  delta(int n1, int m1, int n2, int m2)
-  { return (n1 == n2 && m1 == m2) ? _Tp{1} : _Tp{0}; }
 
 template<typename _Tp>
   void
